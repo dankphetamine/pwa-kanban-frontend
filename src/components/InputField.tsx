ff89ab8e1043a7d26ce220614f8aa68b55983855
Storm K <asge0907@easv365.dk>
2021-03-02 12:12:43 +0100

    Added input field component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,41 @@
+import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
+import { useField } from 'formik';
+import PropTypes from 'prop-types';
+import { FC } from 'react';
+import { InputFieldProps } from '../utils/props';
+import { capitalizeString } from '../utils/text';
+
+export const InputField: FC<InputFieldProps> = ({ type, label, ...props }) => {
+	const [field, { error }] = useField(props.name);
+
+	return (
+		<FormControl isInvalid={!!error} /* casting to boolean */>
+			<FormLabel htmlFor={field.name}>
+				{label ? capitalizeString(field.name.replace(/([A-Z][a-z])/g, ' $1')) : ''}
+			</FormLabel>
+			<Input
+				{...field}
+				{...props}
+				id={field.name}
+				type={type}
+				value={props.value}
+				placeholder={props.placeholder ? capitalizeString(field.name.replace(/([A-Z][a-z])/g, ' $1')) : ''}
+			/>
+			<FormErrorMessage>{error ?? null}</FormErrorMessage>
+		</FormControl>
+	);
+};
+
+InputField.propTypes = {
+	placeholder: PropTypes.bool.isRequired,
+	label: PropTypes.bool.isRequired,
+	name: PropTypes.string.isRequired,
+	type: PropTypes.string,
+	value: PropTypes.string,
+};
+
+InputField.defaultProps = {
+	label: true,
+	placeholder: true,
+	type: 'text',
+};
