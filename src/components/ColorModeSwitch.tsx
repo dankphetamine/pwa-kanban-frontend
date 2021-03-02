dab95f3b824a27fa81f888591f35c77091a9fc0e
Storm K <asge0907@easv365.dk>
2021-03-02 11:55:37 +0100

    renamed darkmodeswitch to colormodeswitch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,9 @@
+import { Switch, useColorMode } from '@chakra-ui/react';
+
+export const ColorModeSwitch = () => {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const isDark = colorMode === 'dark';
+	return (
+		<Switch position="fixed" top="1rem" right="1rem" color="green" isChecked={isDark} onChange={toggleColorMode} />
+	);
+};
