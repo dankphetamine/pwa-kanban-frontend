8ba141c85626b183e6f0d5c7f34974129e2298cf
Storm K <asge0907@easv365.dk>
2021-03-03 13:47:37 +0100

    Added cache to change state on login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,38 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { cacheExchange } from '@urql/exchange-graphcache';
 import { AppProps } from 'next/app';
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
 import NavBar from '../components/NavigationBar';
+import { GetCurrentUserDocument, GetCurrentUserQuery, LoginMutation } from '../graphql/generated/graphql';
 import theme from '../theme';
-import { gqlUrl } from '../utils/constants';
+import { gqlUrl, LoginCacheQuery as AuthCacheQuery } from '../utils/constants';
 
 const client = createClient({
 	url: gqlUrl,
 	fetchOptions: {
 		credentials: 'include',
 	},
+	exchanges: [
+		cacheExchange({
+			updates: {
+				Mutation: {
+					login: (result, _args, cache, _info) => {
+						AuthCacheQuery<LoginMutation, GetCurrentUserQuery>(
+							cache,
+							{ query: GetCurrentUserDocument },
+							result,
+							(res, query) => {
+								if (!res.login.email) return query;
+								else return { getCurrentUser: res.login };
+							},
+						);
+					},
+				},
+			},
+		}),
+		dedupExchange,
+		fetchExchange,
+	],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
