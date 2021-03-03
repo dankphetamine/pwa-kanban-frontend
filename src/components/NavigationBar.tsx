36b60ab09b938d9182becd5eae0189c35e664d83
Storm K <asge0907@easv365.dk>
2021-03-03 12:27:01 +0100

    Added session data to navbar, TODO add state

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,6 @@
-import { Button, chakra, Flex, Stack, useColorMode } from '@chakra-ui/react';
+import { Avatar, Button, chakra, Flex, Stack, useColorMode } from '@chakra-ui/react';
 import { useRouter } from 'next/dist/client/router';
+import { useGetCurrentUserQuery } from '../graphql/generated/graphql';
 import { Colors, Routes } from '../utils/constants';
 import { ColorModeSwitch } from './ColorModeSwitch';
 import Link from './NavigationLink';
@@ -7,6 +8,8 @@ import Link from './NavigationLink';
 const NavBar = () => {
 	const { colorMode } = useColorMode();
 	const router = useRouter();
+	const [{ data }] = useGetCurrentUserQuery();
+
 	return (
 		<chakra.header bgColor={Colors.bgColor[colorMode]} color={Colors.color[colorMode]} w="full" p={4} shadow="lg">
 			<Flex alignItems="center" justifyContent="space-between" mx="auto">
@@ -17,6 +20,26 @@ const NavBar = () => {
 					<Button onClick={() => router.push(Routes.projects)}>
 						<Link href={Routes.projects} />
 					</Button>
+				</Stack>
+				<Stack direction="row" display="flex" alignItems="center" spacing="1rem">
+					{!data?.getCurrentUser && (
+						<Button variant="solid" onClick={() => router.push(Routes.login)}>
+							Sign in | Sign up
+						</Button>
+					)}
+					{data?.getCurrentUser && (
+						<>
+							<Button
+								onClick={() => router.push(Routes.profile)}
+								leftIcon={<Avatar name={data.getCurrentUser.name ?? '? ?'} src={data.getCurrentUser.image!} />}
+							>
+								<Link href={Routes.profile} />
+							</Button>
+							<Button variant="solid" onClick={() => window.confirm('Are you sure you want to sign out?')}>
+								Sign out
+							</Button>
+						</>
+					)}
 				</Stack>
 			</Flex>
 			<ColorModeSwitch />
