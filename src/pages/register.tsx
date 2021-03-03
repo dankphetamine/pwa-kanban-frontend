fab6a830b87ebb0b3fbc2672517a56c19dab24e5
Storm K <asge0907@easv365.dk>
2021-03-03 11:42:21 +0100

    added pseudo error logging on client side for register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,6 @@
 import { Box, Button, Flex, Heading, Stack } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import { useRouter } from 'next/dist/client/router';
-import { ColorModeSwitch } from '../components/ColorModeSwitch';
 import { Container } from '../components/Container';
 import { InputField } from '../components/InputField';
 import { useRegisterMutation } from '../graphql/generated/graphql';
@@ -17,8 +16,18 @@ const Register = () => {
 					<Heading fontSize={'5xl'}>Register account</Heading>
 					<Box rounded={'lg'} boxShadow={'dark-lg'} p={8}>
 						<Formik
-							initialValues={{ email: '', password: '' }}
+							initialValues={{ email: '', password: '', repeatPassword: '' }}
 							onSubmit={async (values, actions) => {
+								if (!values.email.trim().length) return actions.setErrors({ email: 'Email must be included' });
+
+								if (!values.password.trim().length) return actions.setErrors({ password: 'Password must be included' });
+
+								if (!values.repeatPassword.trim().length)
+									return actions.setErrors({ repeatPassword: 'Remember to include me, too' });
+
+								if (values.password !== values.repeatPassword)
+									return actions.setErrors({ password: 'Passwords must match', repeatPassword: '\u200B' });
+
 								const response = await register(values);
 								if (response.error) actions.setErrors({ email: response.error.message });
 								else router.push('/login');
@@ -29,6 +38,7 @@ const Register = () => {
 									<Stack spacing={4}>
 										<InputField name="email" type="email" />
 										<InputField name="password" type="password" />
+										<InputField name="repeatPassword" label={false} type="password" />
 										<Button type="submit" isLoading={isSubmitting} variant="solid">
 											Register
 										</Button>
@@ -39,8 +49,6 @@ const Register = () => {
 					</Box>
 				</Stack>
 			</Flex>
-
-			<ColorModeSwitch />
 		</Container>
 	);
 };
