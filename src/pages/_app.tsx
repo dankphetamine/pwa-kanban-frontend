1be523816e923c03e7d06663dbf6beef51a918d1
Storm K <asge0907@easv365.dk>
2021-03-03 11:43:43 +0100

    added navbar alongside all components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { createClient, Provider } from 'urql';
+import NavBar from '../components/NavigationBar';
 import theme from '../theme';
 import { gqlUrl } from '../utils/constants';
 
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Provider value={client}>
 			<ChakraProvider resetCSS theme={theme}>
+				<NavBar />
 				<Component {...pageProps} />
 			</ChakraProvider>
 		</Provider>
