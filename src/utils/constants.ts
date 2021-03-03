9490d51a3e02ff98a44d2066a1089e4886bc78f3
Storm K <asge0907@easv365.dk>
2021-03-03 11:43:56 +0100

    changed colors, added routes to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,17 @@
 export const Colors = {
-	bgColor: { light: 'messenger.200', dark: 'blue.800' },
+	bgColor: { light: 'messenger.300', dark: 'messenger.800' },
 	color: { light: 'black', dark: 'white' },
+};
+
+export const Routes = {
+	home: '/',
+	// login: '/login', Handled by Next-Auth
+	register: '/register',
+	project: (id: number | string) => `/projects/${id}`,
+	projects: '/projects',
+	profile: '/profile',
+	user: (id: number | string) => `/users/${id}`,
+	users: '/users',
 };
 
 const port = 4000;
