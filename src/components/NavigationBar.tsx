5a4c1f12220d8f28e9a775d02944569d5a6ddb3d
Storm K <asge0907@easv365.dk>
2021-03-06 21:22:56 +0100

    simplified nav bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, chakra, Flex, Skeleton, Stack, useColorMode } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, Skeleton, Stack, useColorMode } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/dist/client/router';
 import { useCurrentUserQuery, useLogOutMutation } from '../graphql/generated/graphql';
@@ -14,45 +14,52 @@ const NavBar = () => {
 	const [, logOut] = useLogOutMutation();
 
 	return (
-		<chakra.header bgColor={Colors.bgColor[colorMode]} color={Colors.color[colorMode]} w="full" p={4} shadow="lg">
-			<Flex alignItems="center" justifyContent="space-between" mx="auto">
-				<Stack direction="row" display="flex" alignItems="center" spacing="1rem">
-					<Button onClick={() => router.push(Routes.home)}>
-						<Link href={Routes.home} text="home" />
-					</Button>
-					<Button onClick={() => router.push(Routes.projects)}>
-						<Link href={Routes.projects} />
-					</Button>
-				</Stack>
-				<Stack direction="row" display="flex" alignItems="center" spacing="2rem">
-					{fetching && (
-						<Box>
-							<Skeleton />
-						</Box>
-					)}
+		<Flex
+			position="fixed"
+			bgColor={Colors.bgColor[colorMode]}
+			color={Colors.color[colorMode]}
+			w="full"
+			p={4}
+			mb="40px"
+			alignItems="center"
+			justifyContent="space-between"
+		>
+			<Stack direction="row" display="flex" alignItems="center" spacing="1rem">
+				<Button onClick={() => router.push(Routes.home)}>
+					<Link href={Routes.home} text="home" />
+				</Button>
+				<Button onClick={() => router.push(Routes.projects)}>
+					<Link href={Routes.projects} />
+				</Button>
+			</Stack>
+			<Stack direction="row" display="flex" alignItems="center" spacing="2rem">
+				{fetching && (
+					<Box>
+						<Skeleton />
+					</Box>
+				)}
 
-					{!fetching && !data?.currentUser && (
-						<Button variant="solid" onClick={() => router.push(Routes.login)}>
-							Sign in | Sign up
+				{!fetching && !data?.currentUser && (
+					<Button variant="solid" onClick={() => router.push(Routes.login)}>
+						Sign in | Sign up
+					</Button>
+				)}
+				{!fetching && data?.currentUser && (
+					<>
+						<Button
+							onClick={() => router.push(Routes.profile)}
+							leftIcon={<Avatar name={data.currentUser.name ?? '? ?'} src={data.currentUser.image!} />}
+						>
+							<Link href={Routes.profile} />
 						</Button>
-					)}
-					{!fetching && data?.currentUser && (
-						<>
-							<Button
-								onClick={() => router.push(Routes.profile)}
-								leftIcon={<Avatar name={data.currentUser.name ?? '? ?'} src={data.currentUser.image!} />}
-							>
-								<Link href={Routes.profile} />
-							</Button>
-							<Button variant="solid" onClick={() => window.confirm('Are you sure you want to sign out?') && logOut()}>
-								Sign out
-							</Button>
-						</>
-					)}
-				</Stack>
-			</Flex>
+						<Button variant="solid" onClick={() => window.confirm('Are you sure you want to sign out?') && logOut()}>
+							Sign out
+						</Button>
+					</>
+				)}
+			</Stack>
 			<ColorModeSwitch />
-		</chakra.header>
+		</Flex>
 	);
 };
 
