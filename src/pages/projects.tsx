6090eb3dc71a8cd423086b4395f8ebc7eeb8ed79
Storm K <asge0907@easv365.dk>
2021-03-18 16:04:45 +0100

    added projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,21 +1,36 @@
-import { Box, SimpleGrid, SkeletonText } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { Box, Flex, IconButton, SimpleGrid, SkeletonText, Spacer } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/dist/client/router';
 import { useState } from 'react';
 import { Container } from '../components/Container';
 import { Header } from '../components/Header';
 import { Main } from '../components/Main';
 import { ProjectCard } from '../components/ProjectCard';
 import { useProjectsQuery } from '../graphql/generated/graphql';
+import { Routes } from '../utils/constants';
 import { createUrqlClient } from '../utils/uqrlUtils';
 
 const Projects = () => {
 	const [variables] = useState({ limit: 5, offset: 0 });
 	const [{ data, fetching }] = useProjectsQuery({ variables: { filter: variables } });
+	const router = useRouter();
 
 	return (
 		<Container width="50">
 			<Main>
-				<Header title="projects" />
+				<Flex>
+					<Header title="projects" />
+					<Spacer />
+					<IconButton
+						aria-label="Search database"
+						icon={<AddIcon />}
+						size="lg"
+						variant="solid"
+						colorScheme="green"
+						onClick={() => router.push(Routes.project_add)}
+					/>
+				</Flex>
 				{fetching && (
 					<Box padding="6" boxShadow="lg">
 						<SkeletonText noOfLines={24} spacing={4} />
