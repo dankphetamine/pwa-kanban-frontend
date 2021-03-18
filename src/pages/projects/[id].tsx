80898c916f95eeca85a767fc19b831ec81f031a3
Storm K <asge0907@easv365.dk>
2021-03-18 16:05:12 +0100

    TODO finalize project-id page drag-n-drop logic

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,12 +1,15 @@
 import { Box, SimpleGrid, SkeletonText } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/dist/client/router';
+import { useState } from 'react';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { Column } from '../../components/Column';
+import { Container } from '../../components/Container';
 import { Header } from '../../components/Header';
 import { Main } from '../../components/Main';
+import { Redirect } from '../../components/Redirect';
 import { useCurrentUserQuery, useProjectQuery } from '../../graphql/generated/graphql';
 import { createUrqlClient } from '../../utils/uqrlUtils';
-import { Container } from '../../components/Container';
-import { Redirect } from '../../components/Redirect';
 
 const Project = () => {
 	const router = useRouter();
@@ -16,12 +19,63 @@ const Project = () => {
 	const [{ data: userData }] = useCurrentUserQuery();
 	const [{ data, fetching }] = useProjectQuery({ variables: { id: parseInt(id) } });
 
-	if (data?.project?.collaborators?.some(u => u.id === userData?.currentUser?.id)) {
-		return Redirect('/');
-	}
+	if (!data?.project?.collaborators?.some(u => u.id === userData?.currentUser?.id)) Redirect('/');
 
-	const tasks = data?.project?.tasks;
-	console.log(tasks);
+	const initialColumns = {
+		todo: {
+			id: 'todo',
+			list: ['item 1', 'item 2', 'item 3'],
+		},
+		doing: {
+			id: 'doing',
+			list: [],
+		},
+		done: {
+			id: 'done',
+			list: [],
+		},
+	};
+
+	const [columns, setColumns] = useState(initialColumns);
+
+	const onDragEnd = ({ source, destination }: DropResult) => {
+		if (destination === undefined || destination === null) return null;
+		if (source.droppableId === destination.droppableId && destination.index === source.index) return null;
+
+		const start = columns[source.droppableId];
+		const end = columns[destination.droppableId];
+
+		if (start === end) {
+			const newList = start.list.filter((_, idx: number) => idx !== source.index);
+
+			newList.splice(destination.index, 0, start.list[source.index]);
+
+			const newCol = { id: start.id, list: newList };
+
+			setColumns(state => ({ ...state, [newCol.id]: newCol }));
+			return null;
+		} else {
+			const newStartList = start.list.filter((_, idx: number) => idx !== source.index);
+
+			const newStartCol = { id: start.id, list: newStartList };
+
+			const newEndList = end.list;
+
+			newEndList.splice(destination.index, 0, start.list[source.index]);
+
+			const newEndCol = {
+				id: end.id,
+				list: newEndList,
+			};
+
+			setColumns(state => ({
+				...state,
+				[newStartCol.id]: newStartCol,
+				[newEndCol.id]: newEndCol,
+			}));
+			return null;
+		}
+	};
 
 	return (
 		<Container width="50">
@@ -32,7 +86,24 @@ const Project = () => {
 					</Box>
 				)}
 				<Header title={`Project ${id}`} />
-				<SimpleGrid columns={4} spacingX="12" spacingY="12" mb="8"></SimpleGrid>
+				<DragDropContext onDragEnd={onDragEnd}>
+					<SimpleGrid columns={3} spacingX="56" spacingY="16" mb="8">
+						{Object.values(columns).map((col, i) => (
+							<Column key={i} name={col.id} tasks={col.list} />
+						))}
+					</SimpleGrid>
+					{/* <SimpleGrid columns={3} spacingX="12" spacingY="12" mb="8">
+						{columnArray.map(txt => {
+							return (
+								<ProjectColumn
+									key={txt}
+									text={capitalizeString(txt)}
+									tasks={data?.project?.tasks?.filter(t => t.status === txt) as Task[]}
+								></ProjectColumn>
+							);
+						})}
+					</SimpleGrid> */}
+				</DragDropContext>
 			</Main>
 		</Container>
 	);
