0eafad3acf066fc40a067ee862e55f893176de6a
Storm K <asge0907@easv365.dk>
2021-03-30 05:54:36 +0200

    changed styling on column, added conditional render

--- src/components/ProjectColumn.tsx
+++ src/components/ProjectColumn.tsx
@@ -6,15 +6,16 @@ import { ProjectTask } from './ProjectTask';
 export const ProjectColumn = ({ text, tasks }: { text: string; tasks: Task[] }) => (
 	<Droppable droppableId={text}>
 		{provided => (
-			<Box w="445px" shadow="xl" rounded="xl" overflow="hidden">
+			<Box w="450px" h="3xl" shadow="dark-lg" rounded="xl" overflow="hidden">
 				<Heading textAlign="center" p={2}>
 					{text}
 				</Heading>
 				<Box px={4} py={8} {...provided.droppableProps} ref={provided.innerRef}>
 					<VStack spacing={2}>
-						{tasks.map((t, i) => {
-							return <ProjectTask key={t.id} task={t as Task} index={i} />;
-						})}
+						{tasks.length &&
+							tasks.map((t, i) => {
+								return <ProjectTask key={t.id} task={t as Task} index={i} />;
+							})}
 					</VStack>
 					{provided.placeholder}
 				</Box>
