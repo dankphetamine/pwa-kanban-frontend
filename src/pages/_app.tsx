453702561a6fa67af3c9911b91553af270b92dfd
Storm K <asge0907@easv365.dk>
2021-03-30 17:04:37 +0200

    added strict mode around entire app (Not sure if it works)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import React from 'react';
 import NavBar from '../components/NavigationBar';
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider resetCSS theme={theme}>
-			<NavBar pageProps={null} />
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<React.StrictMode>
+			<ChakraProvider resetCSS theme={theme}>
+				<NavBar pageProps={null} />
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</React.StrictMode>
 	);
 }
 
