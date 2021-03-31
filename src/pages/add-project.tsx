9f848bec86e41ac00429282df9ee71a5ed5ab09d
Storm K <asge0907@easv365.dk>
2021-03-31 16:36:10 +0200

    Add project finalized (no collaborators yet)

--- src/pages/add-project.tsx
+++ src/pages/add-project.tsx
@@ -1,29 +1,59 @@
-import { Box, SkeletonText } from '@chakra-ui/react';
+import { Box, Button, SimpleGrid, SkeletonText, VStack } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
 import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/dist/client/router';
+import React from 'react';
 import { Container } from '../components/Container';
 import { Header } from '../components/Header';
+import { InputField } from '../components/InputField';
 import { Main } from '../components/Main';
 import { Redirect } from '../components/Redirect';
-import { useCurrentUserQuery } from '../graphql/generated/graphql';
+import { useCreateProjectMutation, useCurrentUserQuery } from '../graphql/generated/graphql';
+import { Routes } from '../utils/constants';
 import { createUrqlClient } from '../utils/uqrlUtils';
 
-const Project = () => {
+const CreateNewProject = () => {
 	const [{ data, fetching }] = useCurrentUserQuery();
+	const router = useRouter();
+	const [, createProject] = useCreateProjectMutation();
 
-	if (!fetching && !data?.currentUser) Redirect('/');
+	if (!fetching && !data?.currentUser) return Redirect('/');
 
 	return (
-		<Container width="50">
+		<Container>
 			<Main>
 				{fetching && (
 					<Box padding="6" boxShadow="lg">
 						<SkeletonText noOfLines={24} spacing={4} />
 					</Box>
 				)}
-				{!fetching && (
+				{!fetching && data?.currentUser && (
 					<>
 						<Header title={'Create a new project'} />
-						<Box></Box>
+						<Box rounded={'lg'} boxShadow={'dark-lg'} p={8}>
+							<Formik
+								initialValues={{ name: '', description: '' }}
+								onSubmit={async (values, actions) => {
+									const response = await createProject(values);
+
+									if (response.error) actions.setErrors({});
+									else router.push(Routes.projects);
+								}}
+							>
+								{({ isSubmitting }) => (
+									<Form>
+										<VStack spacing={4}>
+											<InputField name="name" />
+											<InputField name="description" />
+											<Button type="submit" isLoading={isSubmitting} variant="solid">
+												Create
+											</Button>
+											<SimpleGrid></SimpleGrid>
+										</VStack>
+									</Form>
+								)}
+							</Formik>
+						</Box>
 					</>
 				)}
 			</Main>
@@ -31,4 +61,4 @@ const Project = () => {
 	);
 };
 
-export default withUrqlClient(createUrqlClient)(Project);
+export default withUrqlClient(createUrqlClient)(CreateNewProject);
