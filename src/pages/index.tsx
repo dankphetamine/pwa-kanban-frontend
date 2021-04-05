d246b6c663c31ca2b9a6e9413ec603f50bae3793
Storm K <asge0907@easv365.dk>
2021-04-05 04:09:23 +0200

    removed junk from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,59 +17,9 @@ import { Routes } from '../utils/constants';
 import { createUrqlClient } from '../utils/uqrlUtils';
 
 const Index = () => {
-	// const [variables, setVariables] = useState({ limit: Queries.limitMin, offset: 0 });
-	// const [{ data, fetching }] = useProjectsQuery({ variables: { filter: variables } });
-
 	return (
 		<Container>
 			<Main>
-				{/* <Header />
-		// 		{fetching && (
-		// 			<Box padding="6" boxShadow="lg">
-		// 				<SkeletonText noOfLines={24} spacing={4} />
-		// 			</Box>
-		// 		)}
-		// 		<List spacing={4} my={0}>
-		// 			{!fetching && data?.projects && (
-		// 				<>
-		// 					{data.projects.map(p => (
-		// 						<ListItem key={p.id}>
-		// 							<Text fontSize={'xl'}>{p.name}</Text>
-		// 							<Text fontSize={'small'}>{p.description}</Text>
-		// 						</ListItem>
-		// 					))}
-		// 				</>
-		// 			)}
-		// 		</List>
-		// 		<Flex justifyContent="space-between">
-		// 			<Button
-		// 				onClick={() => {
-		// 					if (data?.projects) {
-		// 						const osVal = Math.max(0, variables.offset - variables.limit);
-
-		// 						setVariables({ limit: variables.limit, offset: osVal });
-		// 					}
-		// 				}}
-		// 				variant="solid"
-		// 				isLoading={fetching}
-		// 				isDisabled={variables.offset < 1}
-		// 			>
-		// 				Go back
-		// 			</Button>
-		// 			<Button
-		// 				onClick={() => {
-		// 					if (data?.projects) {
-		// 						const osVal = Math.max(data?.projects?.length + (variables.offset ?? 0));
-		// 						setVariables({ limit: variables.limit, offset: osVal });
-		// 					}
-		// 				}}
-		// 				variant="solid"
-		// 				isLoading={fetching}
-		// 				isDisabled={(data?.projects?.length ?? 0) < variables.limit}
-		// 			>
-		// 				Go forward
-		// 			</Button>
-		// 		</Flex> */}
 				<ChakraContainer maxW="container.xl" centerContent>
 					<Stack textAlign={'center'} align={'center'}>
 						<Heading fontWeight={600} fontSize="4xl">
