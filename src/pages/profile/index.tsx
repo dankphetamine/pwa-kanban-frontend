44054160a6a350ab98c117bf225254f95ce51ebf
Storm K <asge0907@easv365.dk>
2021-04-24 17:59:30 +0200

    moved profile under profile folder

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,18 +1,15 @@
 import { withUrqlClient } from 'next-urql';
 import { useState } from 'react';
-import { Container } from '../components/Container';
-import { Header } from '../components/Header';
-import { Main } from '../components/Main';
-import { useProjectsQuery } from '../graphql/generated/graphql';
-import { createUrqlClient } from '../utils/uqrlUtils';
+import { Container } from '../../components/Container';
+import { Header } from '../../components/Header';
+import { Main } from '../../components/Main';
+import { useProjectsQuery } from '../../graphql/generated/graphql';
+import { createUrqlClient } from '../../utils/uqrlUtils';
 
 const Projects = () => {
 	const [variables] = useState({ limit: 5, offset: 0 });
 	const [{ data, fetching: _fetching }] = useProjectsQuery({ variables: { filter: variables } });
 	// const [{ data, fetching }] = usePostsQuery({ variables: {} });
-
-	console.log(variables);
-	console.log(data?.projects?.length);
 
 	return (
 		<Container>
