eea0756b604fe7fd981d7135731faae372def0b2
Storm K <asge0907@easv365.dk>
2021-05-20 12:09:52 +0200

    Updated profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,25 +1,58 @@
+import { Box, Heading, HStack, SimpleGrid, Text, VStack } from '@chakra-ui/layout';
 import { withUrqlClient } from 'next-urql';
-import { useState } from 'react';
 import { Container } from '../../components/Container';
 import { Header } from '../../components/Header';
 import { Main } from '../../components/Main';
 import { Redirect } from '../../components/Redirect';
 import { useCurrentUserQuery, useProjectsQuery } from '../../graphql/generated/graphql';
-import { Routes } from '../../utils/constants';
+import { Queries, Routes } from '../../utils/constants';
 import { createUrqlClient } from '../../utils/uqrlUtils';
 
 const Profile = () => {
 	const [{ data: userData, fetching }] = useCurrentUserQuery();
 	if (!fetching && !userData) return <Redirect url={Routes.login} />;
 
-	const [variables] = useState({ limit: 5, offset: 0 });
-	const [{ data, fetching: _fetching }] = useProjectsQuery({ variables: { filter: variables } });
-	// const [{ data, fetching }] = usePostsQuery({ variables: {} });
+	const [{ data, fetching: _fetching }] = useProjectsQuery({ variables: { filter: { limit: Queries.limitMax } } });
 
 	return (
 		<Container>
 			<Main>
-				<Header title="profile" />
+				<Header title="profile & Statistics" />
+
+				<SimpleGrid columns={2} spacing={64}>
+					{!fetching && data?.projects && (
+						<>
+							<Box height="250px">
+								<Heading>Projects</Heading>
+								<VStack>
+									{data?.projects?.map((p, i) => {
+										return (
+											<Box w="445px" shadow="md" rounded="lg" p={6} key={i}>
+												<HStack justifyContent="space-between">
+													<Heading fontSize="lg">{p.name}</Heading>
+												</HStack>
+												<Text>{p.description}</Text>
+											</Box>
+										);
+									})}
+								</VStack>
+							</Box>
+
+							<Box height="250px">
+								<Heading>Tasks</Heading>
+								<VStack>
+									<>
+										{data.projects.forEach(p => {
+											p.tasks?.map((t, i) => {
+												return <Text key={i}>{JSON.stringify(t, null, 2)}</Text>;
+											});
+										})}
+									</>
+								</VStack>
+							</Box>
+						</>
+					)}
+				</SimpleGrid>
 			</Main>
 		</Container>
 	);
